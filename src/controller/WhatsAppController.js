d06c13a42f0d2f1ccf0a2796f8d011d4e4105c25
Everson <eversonpereiraofc@gmail.com>
2022-02-15 15:36:07 -0300

    complementando eventos do menu anexar

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -135,25 +135,65 @@ class WhatsAppController{
         })
 
         this.el.btnAttachPhoto.on("click", e=>{
-            console.log("foto")
+            this.el.inputPhoto.click()
+        })
+
+        this.el.inputPhoto.on("change", e=>{
+            console.log(this.el.inputPhoto.files);
+
+            [...this.el.inputPhoto.files].forEach(file=>{
+                console.log(file)
+            })
         })
 
         this.el.btnAttachCamera.on("click", e=>{
-            console.log("camera")
+            this.closeAllMainPanel()
+            this.el.panelCamera.addClass("open")
+        })
+
+        this.el.btnClosePanelCamera.on("click", e=>{
+            this.closeAllMainPanel()
+            this.el.panelMessagesContainer.show()
+        })
+
+        this.el.btnTakePicture.on("click", e=>{
+            console.log("take picture")
         })
 
         this.el.btnAttachDocument.on("click", e=>{
-            console.log("arquivos")
+            this.closeAllMainPanel()
+            this.el.panelDocumentPreview.addClass("open")
+        })
+
+        this.el.btnSendDocument.on("click", e=>{
+            console.log("send document")
+        })
+
+        this.el.btnClosePanelDocumentPreview.on("click", e=>{
+            this.closeAllMainPanel()
+            this.el.panelMessagesContainer.show("open")
         })
 
         this.el.btnAttachContact.on("click", e=>{
-            console.log("contato")
+            this.el.modalContacts.show()
+        })
+
+        this.el.btnCloseModalContacts.on("click", e=>{
+            this.closeAllMainPanel()
+            this.el.panelMessagesContainer.show()
         })
     }
 
     closeAllLeftPanel(){
         this.el.panelEditProfile.hide()
         this.el.panelAddContact.hide()
+    }
+
+    closeAllMainPanel(){
+        this.el.modalContacts.hide()
+        this.el.panelMessagesContainer.hide()
+        this.el.panelDocumentPreview.removeClass("open")
+        this.el.panelCamera.removeClass("open")
     }
 
     closeMenuAttach(){
